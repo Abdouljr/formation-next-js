4e472567e891bacc9a06a685c56a51bbae722164
Abdouljr <maigaabdouljr@gmail.com>
2025-01-07 21:29:16 +0000

    feat (api): création des end-points getall, add

---
+++ src/app/api/books/route.ts
@@ -0,0 +1,36 @@
+import { prisma } from "@/utils/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const books = prisma.book.findMany();
+    return NextResponse.json(books, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: `Erreur pour la recupération des books ${error}` },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const data = await request.json();
+    const book = await prisma.book.create({
+      data: {
+        titre: data.titre,
+        auteur: data.auteur,
+        reference: data.reference,
+        prix: data.prix,
+        stock: data.stock,
+      },
+    });
+
+    return NextResponse.json(book, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: `Erreur pour la recupération des books ${error}` },
+      { status: 500 }
+    );
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Book } from "@/types/types";
+import { Book, BookFormData } from "@/types/types";
 import { useEffect, useState } from "react";
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -15,8 +15,46 @@ export default function Home() {
   const fetchBooks = async () => {
     try {
       const response = await fetch("/api/books");
+      const data = await response.json();
+      setBooks(data);
     } catch (error) {
       console.error("Une erreur de recupération", error);
+    }
+  };
+
+  const handleAddBook = async (data: BookFormData) => {
+    setIsLoading(true);
+    try {
+      await fetch("/api/books", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      await fetchBooks();
+    } catch (error) {
+      console.error("une erreur pour l'enregistrement ", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleDeleteBook = async (id: number) => {
+    if (!window.confirm("êtes vous sûr de supprimer ce livre ?")) {
+      return;
+    }
+    setIsLoading(true);
+    try {
+      const response = await fetch(`/api/books/${id}`, { method: "DELETE" });
+      if (!response.ok) {
+        throw new Error("Livre non supprimer");
+      }
+
+      await fetchBooks();
+    } catch (error) {
+      console.error("Erreur de suppression");
+    } finally {
+      setIsLoading(false);
     }
   };
   return (
