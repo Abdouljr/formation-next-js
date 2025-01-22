cd16b045e3bce438b98c5a5e67d25e2ab24cb5d8
Abdouljr <maigaabdouljr@gmail.com>
2025-01-22 23:24:22 +0000

    feat (section 2): création et implementation de section 2

--- app/(portail)/page.tsx
+++ app/(portail)/page.tsx
@@ -1,9 +1,11 @@
 import Section1 from "@/components/sections/section1";
+import Section2 from "@/components/sections/section2";
 
 export default function Home() {
   return (
     <div>
       <Section1></Section1>
+      <Section2></Section2>
     </div>
   );
 }
