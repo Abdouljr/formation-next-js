ef3b4cc75dd407544db3223e42c8df34ab92217f
Abdouljr <maigaabdouljr@gmail.com>
2025-01-25 23:38:00 +0000

    fin de la formation

--- app/not-found.tsx
+++
@@ -1,7 +0,0 @@
-export default function notfound() {
-  return (
-    <div className="h-screen flex justify-center items-center">
-      <p className="text-3xl">Page non trouvée 404</p>
-    </div>
-  );
-}
